2faea2a9c7499b4d21ca16ab9fcdd315443d7812
kalininalex <alex@alexkonsoftware.com>
2017-11-15 08:21:54 -0800

    Update AMI

--- docker/install-ami.sh
+++ docker/install-ami.sh
@@ -1,15 +1,14 @@
 #/bin/bash
 
-apt-get update
+sudo apt-get update
 
 curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 bash Anaconda3-4.2.0-Linux-x86_64.sh
 
+export PATH=/home/ubuntu/anaconda3/bin:$PATH
+
 conda create -y -n py27 python=2.7 anaconda
 source activate py27
-
-pip install python-mnist
-pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.3.0-cp27-none-linux_x86_64.whl
 
 conda install -y -c cyclus java-jdk
 
@@ -23,6 +22,9 @@ mkdir dist-spark-2.1.1-scala-2.11.8-linux64-0.2.0-dist
 mv *.zip dist-spark-2.1.1-scala-2.11.8-linux64-0.2.0-dist
 cd dist-spark-2.1.1-scala-2.11.8-linux64-0.2.0-dist
 unzip *.zip
+
+pip install python-mnist
+pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.3.0-cp27-none-linux_x86_64.whl
 
 cd ..
 git clone https://github.com/alex-kalinin/lenet-bigdl
